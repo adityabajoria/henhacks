8ac78b708c21ced846fb364a90cfe91411bd1aea
Ken Tran <khanht@udel.edu>
2024-03-02 18:42:04 -0500

    change date_value

--- Homework/DateFormat.py
+++ Homework/DateFormat.py
@@ -12,9 +12,9 @@ def find_date(date):
     return Date(int(date[0]), int(date[1]), int(date[2]))
 
 # Helper function to determine which date is earlier or later
-def date_value(month: int, day: int, year: int) -> int:
+def date_value(date:Date) -> int:
     """
     31 given for month to ensure 1-31 has less value than 2-1
     405 given for year to ensure 12-31-2023 has less value than 1-1-2024
     """
-    return year * 405 + month * 31 + day
+    return date.year * 405 + date.month * 31 + date.day
