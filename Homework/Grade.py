730d523ada85a029c24a1d107ad667f3c06d023c
adityabajoria <63720245+adityabajoria@users.noreply.github.com>
2024-03-02 16:14:57 -0500

    Update Grade.py

--- Homework/Grade.py
+++ Homework/Grade.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class GradeCategory:
-    homework: int
-    exam: int
-    lab: int
-    participation: int
+    homework: float
+    exam: float
+    lab: float
+    participation: float 
