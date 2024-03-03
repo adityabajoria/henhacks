cfd7a17c2a5ae8bf1ddd25b8d623e91e819ea70d
Aditya Bajoria <aadityaudel.edu>
2024-03-02 21:30:49 -0500

    puhshshh

--- Homework/website.py
+++ Homework/website.py
@@ -3,15 +3,12 @@
 
 from nicegui import ui
 from Course import ken_schedule
-from random import randint
 
 # create a main function that uses nicegui to display only the title as "Updated Canvas Interface" and the body as "Welcome to the updated Canvas Interface"
 # in the ui.body, we are going to display the welcome message to the user. I want to implement the code for get_assignments and get_office_hours in the body of the website interface. 
 # I want to display the assignments and the office hours of the courses in the body of the website interface.
 def main():
-    '''display the title and body of the updated canvas interface'''
-    ui.title('Updated Canvas Interface')
-    ui.body('Welcome to the updated Canvas Interface')
-    ui.run()
+    '''display the main interface of the website along with the title and the body'''
+    
     
     
