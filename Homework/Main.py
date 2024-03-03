1819aa3451d130628e937456c4fd5ebf60feba94
Will Schwantes <willschwantes@gmail.com>
2024-03-02 23:09:39 -0500

    implemented website functionality

--- Homework/Main.py
+++ Homework/Main.py
@@ -3,7 +3,7 @@ from Assignment import Assignment
 from Course import ken_schedule
 
 
-def grab_all_assignment(schedule: list(Course)) -> list(Assignment):
+def grab_all_assignment(schedule: "list[Course]") -> "list[Assignment]":
     assignment_list = []
     for course in ken_schedule:
         for assignment in course.assignments:
