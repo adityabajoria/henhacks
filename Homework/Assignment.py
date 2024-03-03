24fcab1c1ffaa1aa2dee0437d25f1a0874ee054a
Aditya Bajoria <aadityaudel.edu>
2024-03-02 22:12:59 -0500

    get_assignment_due_date fixed

--- Homework/Assignment.py
+++ Homework/Assignment.py
@@ -12,7 +12,7 @@ def get_assignment_due_date(ken_schedule):
     '''get the course with the earliest due date'''
     assignments = []
     for assignment in ken_schedule:
-        assignments.append(assignment)
+        assignments.append(assignment.name, assignment.due_date, assignment.point, assignment.types)
     assignments.sort(key=lambda x: x.due_date) # sorts the assignments by due date
     return assignments
 
