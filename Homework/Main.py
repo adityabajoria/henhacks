b4cf6fc99adf292c932ecccc7a51d2a6e01e38bb
Aditya Bajoria <aadityaudel.edu>
2024-03-02 19:00:25 -0500

    point done

--- Homework/Main.py
+++ Homework/Main.py
@@ -1,2 +1,9 @@
 from Course import ken_schedule
+from nicegui import ui
+from random import randint
 
+# create a main function that uses nicegui to display only the title as "Updated Canvas Interface" and the body as "Welcome to the updated Canvas Interface"
+def main():
+    '''display the title and body of the updated canvas interface'''
+    
+    
